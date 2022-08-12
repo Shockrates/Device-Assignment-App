c023e1dd641c586f0d7d8cee3afce3a56b126a12
Shockrates <blackhound55@gmail.com>
2022-08-12 14:35:26 +0300

    Added add deviceType functionality

--- frontend/src/app/deviceType/device-type-input/device-type-input.component.ts
+++ frontend/src/app/deviceType/device-type-input/device-type-input.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DeviceType } from 'src/app/models/device-type.model';
+import { DataStoreService } from 'src/app/services/data-store.service';
+import { DeviceTypeService } from 'src/app/services/device-type.service';
+import { uniqueDeviceTypeNameValidator } from 'src/app/shared/custom-validation';
 
 @Component({
     selector: 'app-device-type-input',
@@ -6,11 +11,49 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./device-type-input.component.scss']
 })
 export class DeviceTypeInputComponent implements OnInit {
+    @Input()
+    deviceType!: DeviceType;
     public value: string = '';
+    deviceTypeForm: FormGroup | any;
 
-    constructor() {}
+    constructor(private formBuilder: FormBuilder, private deviceTypeService: DeviceTypeService, private dataService: DataStoreService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.buildDeviceTypeForm();
+    }
 
-    submit() {}
+    get deviceTypeNameControls() {
+        return this.deviceTypeForm.controls['name'];
+    }
+
+    buildDeviceTypeForm() {
+        this.deviceTypeForm = this.formBuilder.group({
+            name: [
+                '',
+                {
+                    validators: [Validators.required],
+                    asyncValidators: [uniqueDeviceTypeNameValidator(this.deviceTypeService, this.deviceType?.name)],
+                    updateOn: 'blur'
+                }
+            ]
+        });
+    }
+
+    addDeviceType() {
+        if (this.deviceTypeForm.valid) {
+            this.deviceTypeService.createDeviceType(this.deviceTypeForm.value).subscribe({
+                next: (res) => {
+                    this.deviceTypeForm.reset();
+                    this.notifyForChange();
+                },
+                error: (err) => {
+                    console.log(err);
+                }
+            });
+        }
+    }
+
+    notifyForChange() {
+        this.dataService.notifyAboutChange();
+    }
 }
--- frontend/src/app/shared/custom-validation.ts
+++ frontend/src/app/shared/custom-validation.ts
@@ -1,6 +1,6 @@
 import { AbstractControl, AsyncValidatorFn } from '@angular/forms';
-import { map, Observable } from 'rxjs';
-import { Employee } from '../models/employee.model';
+import { map } from 'rxjs';
+import { DeviceTypeService } from '../services/device-type.service';
 import { DeviceService } from '../services/device.service';
 import { EmployeeService } from '../services/employee.service';
 
@@ -9,7 +9,7 @@ export function uniqueEmailValidator(employeeService: EmployeeService, email: st
         return employeeService.getAllEmployees().pipe(
             map((res) => {
                 const employee = res.employees.find((employee) => employee.email.toLowerCase() == c.value.toLowerCase());
-                return (employee && employee.email !== email) ? { emailExists: true } : null;
+                return employee && employee.email !== email ? { emailExists: true } : null;
             })
         );
     };
@@ -20,7 +20,18 @@ export function uniqueDeviceSerialValidator(deviceService: DeviceService, serial
         return deviceService.getAllDevices().pipe(
             map((res) => {
                 const device = res.devices.find((device) => device.serialNumber.toLowerCase() == c.value.toLowerCase());
-                return (device && device.serialNumber !== serial) ? { serialExists: true } : null;
+                return device && device.serialNumber !== serial ? { serialExists: true } : null;
+            })
+        );
+    };
+}
+
+export function uniqueDeviceTypeNameValidator(deviceTypeService: DeviceTypeService, name: string): AsyncValidatorFn {
+    return (c: AbstractControl) => {
+        return deviceTypeService.getAllDeviceTypes().pipe(
+            map((res) => {
+                const deviceType = res.deviceTypes.find((deviceType) => deviceType.name.toLowerCase() == c.value.toLowerCase());
+                return deviceType && deviceType.name !== name ? { nameExists: true } : null;
             })
         );
     };
